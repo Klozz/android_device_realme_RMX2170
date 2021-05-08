729a3199d2048e1b80c716d6ddc43c3197fe3a0b
Retypolkg <damian11vp.pl@gmail.com>
2021-05-08 14:44:56 +0200

    X2: Update to redfin May fp

--- init/x2.cpp
+++ init/x2.cpp
@@ -90,24 +90,24 @@ void setRMX(const unsigned int variant)
 
     //RMX1991
     prop[0] = {
-        "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys",
-        "redfin-user 11 RQ2A.210405.005 7181113 release-keys",
+        "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys",
+        "redfin-user 11 RQ2A.210505.003 7255357 release-keys",
         "RMX1991",
         "RMX1991CN",
     };
 
     //RMX992
     prop[1] = {
-        "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys",
-        "redfin-user 11 RQ2A.210405.005 7181113 release-keys",
+        "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys",
+        "redfin-user 11 RQ2A.210505.003 7255357 release-keys",
         "RMX1992",
         "RMX1992L1",
     };
 
     //RMX1993
     prop[2] = {
-        "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys",
-        "redfin-user 11 RQ2A.210405.005 7181113 release-keys",
+        "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys",
+        "redfin-user 11 RQ2A.210505.003 7255357 release-keys",
         "RMX1993",
         "RMX1993L1",
     };
--- lineage_X2.mk
+++ lineage_X2.mk
@@ -19,6 +19,6 @@ PRODUCT_MANUFACTURER := realme
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
