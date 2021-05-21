33d6335baec01dff5e4ab4b23f52891d67860277
XsaviXander <XsaviXander@users.noreply.github.com>
2021-05-21 06:20:58 +0000

    X2: Enable freeform windows

--- device.mk
+++ device.mk
@@ -107,7 +107,11 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhwbinder
 
-#Hardware
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
+# Hardware
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:system/etc/permissions/android.hardware.telephony.ims.xml
